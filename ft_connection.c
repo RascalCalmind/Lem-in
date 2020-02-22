1dd1abc5796b9e7430c4a5505d5206a307f08915
Lotte Hageman <lhageman@f1r4s10.codam.nl>
2020-02-22 14:39:56 +0100

    memory allocation edges fixed

--- ft_connection.c
+++ ft_connection.c
@@ -6,7 +6,7 @@
 /*   By: lhageman <lhageman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/07 13:00:40 by lhageman       #+#    #+#                */
-/*   Updated: 2020/02/21 15:40:46 by lhageman      ########   odam.nl         */
+/*   Updated: 2020/02/22 14:33:13 by lhageman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,16 +95,23 @@ int		ft_assign_connection(char **arr, t_lemin *list)
 	// b->link_count = i;
 	if (a->edges == NULL)
 		a->edges = (t_edge **)ft_memalloc(sizeof(t_edge *) * (list->rooms + 1));
+	ft_printf("FT_ASS_CONN --- allocated edges for a \n");
 	while (a->edges[i] != NULL)
 		i += 1;
+	ft_printf("FT_ASS_CONN --- i to assign: %i\n", i);
+	a->edges[i] = (t_edge *)ft_memalloc(sizeof(t_edge *));
+	//a->edges[i]->to = (t_room *)ft_memalloc(sizeof(t_room *));
 	a->edges[i]->to = b;
 	a->edges[i]->available = 1;
 	a->link_count = i;
 	i = 0;
 	if (b->edges == NULL)
 		b->edges = (t_edge **)ft_memalloc(sizeof(t_edge *) * (list->rooms + 1));
+	ft_printf("FT_ASS_CONN --- allocated edges for b\n");
 	while (b->edges[i] != NULL)
 		i += 1;
+	b->edges[i] = (t_edge *)ft_memalloc(sizeof(t_edge *));
+	//b->edges[i]->to = (t_room *)ft_memalloc(sizeof(t_room *));
 	b->edges[i]->to = a;
 	b->edges[i]->available = 1;
 	b->link_count = i;
