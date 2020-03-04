ad4f82888f2d1386b51cb8b2942bb5fc603507f3
Lotte Hageman <lhageman@f1r4s10.codam.nl>
2020-03-04 18:13:10 +0100

    first draft print ants

--- ft_ants.c
+++ ft_ants.c
@@ -6,7 +6,7 @@
 /*   By: lhageman <lhageman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/02/24 15:53:03 by lhageman       #+#    #+#                */
-/*   Updated: 2020/03/04 15:28:34 by lhageman      ########   odam.nl         */
+/*   Updated: 2020/03/04 17:30:02 by lhageman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,16 +70,67 @@
 // 	return (0);
 // }
 
-static t_path *find_best_path(t_lemin *lemin) {
-	int i;
-	t_path *shortest;
+static int		ft_print_ants(t_lemin *lemin)
+{
+	int p;
+	int a;
+	int cur;
+	int prev;
+	int test;
+
+	p = 0;
+	prev = -1;
+	test = -1;
+	while (p < lemin->path_count)
+	{
+		a = 0;
+		while (a < lemin->paths[p]->ants_len)
+		{
+			//ft_printf("Getting data for path %i, ant %i\n", p, a);
+
+			cur = lemin->paths[p]->ants[a]->cur;
+			//ft_printf("current location for ant %i on path %i is %i\n", a, p, cur);
+			if (prev != cur && cur < lemin->paths[p]->len)
+			{
+				ft_printf("L%i-%s",lemin->paths[p]->ants[a]->num, lemin->paths[p]->room[cur]->name);
+				lemin->paths[p]->ants[a]->cur += 1;
+				prev = cur;
+				if (p + 1 < lemin->path_count)
+					ft_printf(" ");
+				test = 0;
+			}
+			else if (prev != cur && cur == lemin->paths[p]->len)
+			{
+				ft_printf("L%i-%s",lemin->paths[p]->ants[a]->num, lemin->end);
+				lemin->paths[p]->ants[a]->cur += 1;
+				prev = cur;
+				if (p + 1 < lemin->path_count)
+					ft_printf(" ");
+				test = 0;
+			}
+			a += 1;
+		}
+		prev = -1;
+		p += 1;
+	}
+	if (p > 0)
+		ft_printf("\n");
+	if (test == 0)
+		return (0);
+	return (1);
+}
+
+static t_path	*find_best_path(t_lemin *lemin)
+{
+	int		i;
+	t_path	*shortest;
 
 	i = 0;
 	shortest = lemin->paths[0];
-
-	while(i < lemin->path_count)
+	while (i < lemin->path_count)
 	{
-		if ((lemin->paths[i] != NULL && (shortest->len + shortest->ants_len) > (lemin->paths[i]->len + lemin->paths[i]->ants_len))) {
+		if ((lemin->paths[i] != NULL && (shortest->len + shortest->ants_len) > (lemin->paths[i]->len + lemin->paths[i]->ants_len))) 
+		{
 			shortest = lemin->paths[i];
 			printf("[BEST PATH] Found new best path! %p %i\n", &shortest, i);
 		}
@@ -88,26 +139,31 @@ static t_path *find_best_path(t_lemin *lemin) {
 	return (shortest);
 }
 
-int		place_ants(t_lemin *lemin) 
+int				place_ants(t_lemin *lemin)
 {
-	t_path *path;
-	t_ant *ant;
-	int i;
+	t_path	*path;
+	t_ant	*ant;
+	int		i;
+	int		ret;
 
 	i = 0;
-
+	ret = -1;
 	while (i < lemin->ants)
 	{
-		printf("[PLACE ANTS] Placing ant: %i\n", i);
+		printf("[PLACE ANTS] Placing ant: %i\n", i + 1);
 		ant = (t_ant *)ft_memalloc(sizeof(t_ant));
 		ant->cur = 0;
-		ant->num = i;
+		ant->num = i + 1;
 		path = find_best_path(lemin);
-		printf("[PLACE ANTS] Found best path at! (%p) len:%i\n", &path);
+		printf("[PLACE ANTS] Found best path at! (%p) len:%i\n", &path, path->ants_len);
 		path->ants[path->ants_len] = ant;
 		path->ants_len++;
 		i++;
 	}
+	ft_printf("Calculated all routes for ants! now printing:\n");
+	while (ret != 1)
+		ret = ft_print_ants(lemin);
+	return (0);
 }
 
 // int		ants_test(void)
